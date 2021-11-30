9e0fa9d44c1655757a6b809e4515e66e1ff495ab
Emalongi <91636607+Emalongi@users.noreply.github.com>
2021-11-30 11:03:38 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,19 +1,13 @@
-//
-// Created by Utente on 29/11/2021.
-//
-
 #include "lib.h"
-bool numeriprimi (int a=2,int b=1){
-
-    if(a==2){
+bool primi(int numero, int divisore){
+    if(numero<0){
+        return false;
+    }else if(divisore==1){
         return true;
-    }else if(a>2 and b<a){
-        if(a%(a-b)==0){
-            return false;
-        }else if(b<a){
-            numeriprimi(a,++b);
-            return true;
-        }
-
+    }else if(numero%divisore==0){
+        return false;
     }
+    primi(numero,--divisore);
+
 }
+
