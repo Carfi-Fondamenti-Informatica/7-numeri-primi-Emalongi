1c025fe08b9ebf29fac7cfbc684560385c6710e4
Emalongi <91636607+Emalongi@users.noreply.github.com>
2021-11-30 09:27:12 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
+//
+// Created by Utente on 29/11/2021.
+//
 
+#include "lib.h"
+bool numeriprimi (int a=2,int b=1){
+
+    if(a==2){
+        return true;
+    }else if(a>2 and b<a){
+        if(a%(a-b)==0){
+            return false;
+        }else if(b<a){
+            numeriprimi(a,++b);
+            return true;
+        }
+
+    }
+}
